8af82eb6f252aa458c6a22273b96fefc44e248be
Lucy <camilaluciasosa@hotmail.com>
2024-04-27 22:11:52 -0300

    feat: alternative routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,19 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/bienvenida", to: "home#index"
+
   root to: "home#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/new", to: "articles#new", as: :new_articles
-  get "/articles/:id", to: "articles#show"
-  get "/articles/:id/edit", to: "articles#edit"
+  resources :articles do
+    get "/user/:user_id", to: "articles#from_author", on: :collection
+  end
 
-  get "/articles/user/:user_id", to: "articles#from_author"
-  
-  patch "/articles/:id", to: "articles#update", as: :article
-  post "/articles", to: "articles#create"
-  delete "/articles/:id", to: "articles#destroy"
+  # get "/articles", to: "articles#index"
+  # get "/articles/new", to: "articles#new", as: :new_articles
+  # get "/articles/:id", to: "articles#show"
+  # get "/articles/:id/edit", to: "articles#edit"
+  # patch "/articles/:id", to: "articles#update", as: :article
+  # post "/articles", to: "articles#create"
+  # delete "/articles/:id", to: "articles#destroy"
+
 end
