fd7e5600311228ed7a311a822e334f0d86cb8adb
Lucy <camilaluciasosa@hotmail.com>
2024-04-30 18:19:19 -0300

    feat: create category scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_26_194211) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_29_202027) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -56,6 +56,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_194211) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
